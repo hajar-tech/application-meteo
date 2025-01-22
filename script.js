ed14bc80994d7353d9fd88fc08e57482c8bd8519
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-01-22 18:00:06 +0100

    started function gatWeather()

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const apiKey = '';
-const apiUrl = '';
+const apiKey = '29e9dfb943b9e5cd7938a503cf15babc';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=marrakech&units=metric';
+
+async function getWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
+
+getWeather();
