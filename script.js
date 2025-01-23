2c9972eec57c37e975c81cbaaa199eb1c00f04ae
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-01-23 17:26:24 +0100

    affichage des infos principale avec api

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const apiKey = '29e9dfb943b9e5cd7938a503cf15babc';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=marrakech&units=metric';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+// const daysApi = 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric';
+const searchCity=document.querySelector('.bar-search .search');
+const searchBtn = document.querySelector('.btn')
 
-async function getWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function getWeather(city) {
+    const response = await fetch(apiUrl +city+`&appid=${apiKey}`);
     var data = await response.json();
+    document.querySelector('.name-city').innerHTML=data.name;
+    document.querySelector('.temp').innerHTML=Math.round(data.main.temp)+" °C";
+    document.querySelector('.humidity').innerHTML=data.main.humidity+" %";
+    document.querySelector('.wind').innerHTML=data.wind.speed+" km/h";
 
     console.log(data);
-}
 
-getWeather();
+    // const forcastResponse = await fetch()
+}
+ searchBtn.addEventListener('click', ()=>{
+    getWeather(searchCity.value.trim() );
+ })
+
+
