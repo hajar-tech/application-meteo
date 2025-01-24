e6fb0beaf815a650625f29f4ca792af7e000039f
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-01-24 18:02:20 +0100

    start position

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = '29e9dfb943b9e5cd7938a503cf15babc';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const daysApi = 'https://api.openweathermap.org/data/2.5/forecast?units=metric&q=';
-const locleUrl =
+const localeUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}';
 const searchCity=document.querySelector('.bar-search .search');
 const searchBtn = document.querySelector('.btn')
 const days = document.getElementsByClassName('footer');
@@ -40,7 +40,38 @@ async function getFiveDaysWeather(city) {
      })
 
 //fonction pour la geolocalisation
+async function geoFindMe() {
+     const positionData = await fetch(localeUrl +`&appid=${apiKey}`);
+     const position = await positionData.json();
+     console.log(position);
+    const status = document.querySelector("#status");
+    const mapLink = document.querySelector("#map-link");
   
+    mapLink.href = "";
+    mapLink.textContent = "";
+  
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = "";
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = "Unable to retrieve your location";
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = "Geolocation is not supported by your browser";
+    } else {
+      status.textContent = "Locating…";
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }
+  
+  document.querySelector("#find-me").addEventListener("click", geoFindMe);
       
 
 
